74c5370e8f789a6d6f91b024fff9224f16cb6f27
BuQian <buqian0627@163.com>
2019-08-03 07:13:24 +0800

    12

--- 12/js/toast.js
+++ 12/js/toast.js
@@ -1,5 +1,5 @@
 ;(function ($) {
-  let settings, cancelBtn, okBtn, closeBtn, timer
+  let settings, timer
 
   let _renderDOM = function() {
     if( $('.toast').length > 0){
@@ -8,47 +8,17 @@
 
     clearTimeout(timer)
 
-    $('body').append(messageBox = $('<div class="message-box"></div>'))
-    messageBox.append(
-      messageBoxShade = $('<div class="message-box-shade"></div>')
-    )
-    messageBox.append(
-      messageBoxContent = $('<div class="message-box-content"></div>')
-    )
+    $('body').append(toast = $('<div class="toast ' + settings.type + '"></div>'))
+    toast.append('<i></i>')
 
-
-    // messageBox
-    messageBoxContent.append(header = $('<header class="message-box-header"></header>'))
-    header.append('<h4>' + settings.title + '</h4>')
-    header.append(closeBtn = $('<span></span>'))
-
-    messageBoxContent.append('<section class="message-box-body">\n' +
-      '      <p>'+ settings.message +'</p>\n' +
-      '    </section>')
-
-    messageBoxContent.append(footer = $('<footer class="message-box-footer"></footer>'))
-    footer.append(cancelBtn = $('<button class="btn btn-cancel">' + settings.cancelBtn + '</button>'))
-    footer.append(okBtn = $('<button class="btn btn-ok">'+ settings.okBtn +'</button>'))
-
-
-    setTimeout(function(){
-      messageBox.addClass('message-box-show')
-    }, 20);
+    toast.append('<span>' + settings.message + '</span>')
 
   }
 
   let _bindEvent = function() {
-    okBtn.on('click', function () {
-      settings.okFun()
-      $.messageBox.close()
-    })
-    cancelBtn.on('click', function () {
-      settings.cancelFun()
-      $.messageBox.close()
-    })
-    closeBtn.on('click', function () {
-      $.messageBox.close()
-    })
+    timer = setTimeout(function(){
+      toast.remove()
+    }, 5000);
   }
 
 
@@ -56,39 +26,25 @@
 
 
 
-  $.messageBox = function(options) {
-    settings = $.extend({}, $.fn.messageBox.defaults, options);
-    $.messageBox.init();
+  $.toast = function(options) {
+    settings = $.extend({}, $.fn.toast.defaults, options);
+    $.toast.init();
     return this
   }
 
-  $.messageBox.init = function() {
+  $.toast.init = function() {
     _renderDOM()
     _bindEvent()
   }
 
-  // 弹窗关闭
-  $.messageBox.close = function() {
-    messageBox.removeClass('message-box-show')
-    timer = setTimeout(function(){
-      messageBox.remove()
-    }, 200);
-  }
-
   // 插件
-  $.fn.messageBox = function(){
+  $.fn.toast = function(){
     return this
   };
 
-  $.fn.messageBox.defaults = {
-    title: 'VIP功能提醒',
-    message: '该服务是VIP专属，您确定要了解么？',
-
-    cancelBtn: '返回',
-    okBtn: '去了解',
-
-    okFun: function () {},
-    cancelFun: function () {}
+  $.fn.toast.defaults = {
+    type: 'loading',
+    message: '信息获取中'
   }
 
 })(jQuery)
